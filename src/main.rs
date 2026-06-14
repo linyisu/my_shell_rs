7f78524d5347a5639247c2920380e66de2f1f03d
linyisu <linyisu1024@163.com>
2026-06-15 01:49:29 +0800

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,72 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
 
+struct Command {
+    name: String,
+    command: String,
+    args: Vec<String>,
+}
+
+impl Command {
+    fn new() -> Self {
+        Self {
+            args: Vec::new(),
+            name: String::new(),
+            command: String::new(),
+        }
+    }
+
+    fn parse(&mut self) {
+        let mut command = String::new();
+        io::stdin().read_line(&mut command).unwrap();
+
+        self.command = String::from(command.trim());
+        self.args = self
+            .command
+            .split_whitespace()
+            .map(|s| s.to_string())
+            .collect();
+        self.name = self.args[0].clone();
+        self.args.remove(0);
+    }
+}
+
+enum Builtin {
+    Type,
+    Exit,
+    Echo,
+}
+
+impl Builtin {
+    fn from_str(s: &str) -> Option<Self> {
+        match s {
+            "type" => Some(Self::Type),
+            "exit" => Some(Self::Exit),
+            "echo" => Some(Self::Echo),
+            _ => None,
+        }
+    }
+}
+
 fn main() {
     loop {
         print!("$ ");
         io::stdout().flush().unwrap();
 
-        let mut command = String::new();
-        io::stdin().read_line(&mut command).unwrap();
+        let mut command = Command::new();
+        command.parse();
 
-        let command_args: Vec<String> = command
-            .trim()
-            .split_whitespace()
-            .map(|s| s.to_string())
-            .collect();
-
-        match command_args[0].as_str() {
+        match command.name.as_str() {
+            "type" => {
+                if let Some(_) = Builtin::from_str(command.args[0].as_str()) {
+                    println!("{} is a shell builtin", command.args[0]);
+                } else {
+                    println!("{}: command not found", command.args[0]);
+                }
+            }
             "exit" => break,
-            "echo" => println!("{}", &command.trim()[5..]),
-            _ => println!("{}: command not found", command.trim()),
+            "echo" => println!("{}", &command.command[5..]),
+            _ => println!("{}: command not found", command.command),
         }
     }
 }
