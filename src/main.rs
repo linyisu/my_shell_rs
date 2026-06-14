5540adde215f2cdc202632500783e41f36ee07b4
linyisu <linyisu1024@163.com>
2026-06-15 00:55:02 +0800

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -4,4 +4,8 @@ use std::io::{self, Write};
 fn main() {
     print!("$ ");
     io::stdout().flush().unwrap();
+
+    let mut command = String::new();
+    io::stdin().read_line(&mut command).unwrap();
+    println!("{}: command not found", command.trim());
 }
