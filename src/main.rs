7b4daa79c0bb3a28784833c41d25763e689b8ded
linyisu <linyisu1024@163.com>
2026-06-15 01:22:20 +0800

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,15 @@ fn main() {
         let mut command = String::new();
         io::stdin().read_line(&mut command).unwrap();
 
-        match command.trim() {
+        let command_args: Vec<String> = command
+            .trim()
+            .split_whitespace()
+            .map(|s| s.to_string())
+            .collect();
+
+        match command_args[0].as_str() {
             "exit" => break,
+            "echo" => println!("{}", &command.trim()[5..]),
             _ => println!("{}: command not found", command.trim()),
         }
     }
